a980624f1c8d0d4d1f2feadd5a857ae240b1aea5
Jonathan Ruiz <jonathanruiz@MacBook-Air-de-Nerd.local>
2024-01-12 14:17:55 -0300

    lazy agregado en App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,82 @@
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-import Divider from './components/Divider';
 import Footer from './components/Footer';
-import Header from './components/Header';
-import Hero from './components/Hero';
+import Spinner from './components/Spinner';
+
+const LazyHero = React.lazy(() => import('./components/Hero'));
+const LazyDivider = React.lazy(() => import('./components/Divider'));
+const LazyHeader = React.lazy(() => import('./components/Header'));
+
+const LazyComponent = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect(); // Stop observing once it's visible
+        }
+      },
+      {
+        root: null, // Use the viewport as the root
+        rootMargin: '0px',
+        threshold: 0.2,
+      }
+    );
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    };
+  }, []);
+
+  return <div ref={elementRef}>{isVisible && children}</div>;
+};
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
   return (
-    <div className="App">
-      <Header/>
-      <Hero/>
-      {/* <Divider/> */}
-      <Footer/>
-      
+    <div className={`App ${isLoading ? 'loading' : ''}`}>
+      {isLoading ? (
+        <div className="loading-overlay">
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          <LazyComponent>
+            <React.Suspense fallback={<Spinner />}>
+              <LazyHeader />
+            </React.Suspense>
+          </LazyComponent>
+          <LazyComponent>
+            <React.Suspense fallback={<Spinner />}>
+              <LazyHero />
+            </React.Suspense>
+          </LazyComponent>
+          <LazyComponent>
+            <React.Suspense fallback={<Spinner />}>
+              <LazyDivider />
+            </React.Suspense>
+          </LazyComponent>
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
--- src/components/Divider.js
+++ src/components/Divider.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Divider = () => {
   return (
       <div className="h-1 bg-gray-200 rounded overflow-hidden">
-          <div className="w-24 h-full bg-indigo-500"></div>
+          <div className="w-24 h-full "></div>
       </div>
   )
 }
